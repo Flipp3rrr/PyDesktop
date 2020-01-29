97ff413cb693f1bf06e8f990711277d8d4767d72
Flipp3rrr <philipkreuning@icloud.com>
2020-01-29 11:17:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print("?> Booting PythonOS...")
 print("?> Starting modules...")
 import tkinter as tk
 import sys
+import time
 print("> Modules started!")
 print("> PythonOS booted succesfully!")
 print("> Looking for desktop environment...")
@@ -14,5 +15,15 @@ print("Welcome to PythonOS!")
 while True:
     cmd = input("BasicSHELL> ")
     
-    if cmd == "exit":
+    if cmd == "help":
+        print("Basic commands:\n  help: display help text\n  exit: stop PythonOS\n  fileXPLR: File explorer")
+
+    if cmd == "shutdown":
+        print("Shutting down...")
         sys.exit()
+
+    if cmd == "ls":
+        print("The current directory contains the following:\nEMPTY")
+
+    else:
+        print("ERR_1:", cmd, "is not a valid command!")
